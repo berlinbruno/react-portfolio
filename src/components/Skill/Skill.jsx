1615c04bf6dfabb0e3a030d15938910964203165
Berlin Bruno <132148418+berlinbruno@users.noreply.github.com>
2023-11-26 12:52:07 +0530

    UI Changes

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -10,7 +10,7 @@ export const Skill = () => {
       <h2 className={styles.title}>Skills</h2>
       <div className={styles.content}>
         <div className={styles.innerContent}>
-          <h1>Front End</h1>
+          <h3>Front End</h3>
           <div className={styles.skills}>
             {skills.frontend.map((skill, id) => {
               return (
@@ -25,7 +25,7 @@ export const Skill = () => {
           </div>
         </div>
         <div className={styles.innerContent}>
-          <h1>Back End</h1>
+          <h3>Back End</h3>
           <div className={styles.skills}>
             {skills.backend.map((skill, id) => {
               return (
@@ -40,7 +40,7 @@ export const Skill = () => {
           </div>
         </div>
         <div className={styles.innerContent}>
-          <h1>Database</h1>
+          <h3>Database</h3>
           <div className={styles.skills}>
             {skills.database.map((skill, id) => {
               return (
@@ -55,7 +55,7 @@ export const Skill = () => {
           </div>
         </div>
         <div className={styles.innerContent}>
-          <h1>Others</h1>
+          <h3>Others</h3>
           <div className={styles.skills}>
             {skills.others.map((skill, id) => {
               return (
